41fe12572dc65f20cade3764efc9b8d1890b2ab3
Volodymyr Sudomliak <volodymyrsudomliak@gmail.com>
2023-07-20 10:46:57 +0300

    Replaced 4 and 5 with SHIFT_ON_4 and SHIFT_ON_5

--- src/Keys/BruteForce.java
+++ src/Keys/BruteForce.java
@@ -11,17 +11,17 @@ public class BruteForce extends Key{
         for (int i = 0; i < fileText.length() - minSubstringLength; i++) {
             for (int j = i + minSubstringLength; j < fileText.length(); j++) {
                 String substring = fileText.substring(i, j);
-                if (fileText.indexOf(substring, j) != -1) {
+                if (fileText.indexOf(substring, j) != Constants.LAST_POINT) {
                     if (substring.equals(Constants.KEY_DECODING_3_SPACES)) {
                         shiftLeft(Constants.SHIFT_ON_3);
                         return fileText.trim().replace(Constants.KEY_DECODING_3_SPACES, Constants.SPACE_3_SYMBOLS_RIGHT);
                     }
                     if (substring.equals(Constants.KEY_DECODING_4_SPACES)) {
-                        shiftLeft(4);
+                        shiftLeft(Constants.SHIFT_ON_4);
                         return fileText.trim().replace(Constants.KEY_DECODING_4_SPACES, Constants.SPACE_4_SYMBOLS_RIGHT);
                     }
                     if (substring.equals(Constants.KEY_DECODING_5_SPACES)) {
-                        shiftLeft(5);
+                        shiftLeft(Constants.SHIFT_ON_5);
                         return fileText.trim().replace(Constants.KEY_DECODING_5_SPACES, Constants.SPACE_5_SYMBOLS_RIGHT);
                     }
                 }
